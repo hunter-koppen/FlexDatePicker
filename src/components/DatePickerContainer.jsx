f00070b0565cc73ee8933b230a41c905f3dad21c
Hunter Koppen <hkoppen@clevr.com>
2022-08-02 00:34:47 +0200

    first draft

--- src/components/DatePickerContainer.jsx
+++ src/components/DatePickerContainer.jsx
@@ -1,4 +1,7 @@
 import { Component, createElement } from "react";
+import moment_ from "moment";
+const moment = moment_;
+
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
@@ -9,30 +12,39 @@ export class DatePickerContainer extends Component {
         editedvalue: null,
         placeholder: null,
         firstDayOfWeek: null,
-        locale: null
+        locale: null,
+        open: false,
+        dateFormat: null,
     };
 
     componentDidMount() {
-        const days = mx.session.sessionData.locale.dates.shortWeekdays;
+        const eras = mx.session.sessionData.locale.dates.eras;
+        const quarters = ["1", "2", "3", "4"];
         const months = mx.session.sessionData.locale.dates.months;
+        const days = mx.session.sessionData.locale.dates.shortWeekdays;
+        const dayPeriods = mx.session.sessionData.locale.dates.dayPeriods;
         const locale = {
             localize: {
+                era: n => eras[n],
+                quarter: n => quarters[n],
+                month: n => months[n],
                 day: n => days[n],
-                month: n => months[n]
+                dayPeriod: n => dayPeriods[n]
             },
             formatLong: {
                 date: () => mx.session.sessionData.locale.patterns.date
-            }
+            },
+            match: {}
         };
 
         this.setState({
             firstDayOfWeek: mx.session.sessionData.locale.firstDayOfWeek,
-            locale: locale
+            locale: locale,
+            dateFormat: mx.session.sessionData.locale.patterns.date
         });
     }
 
     componentDidUpdate(prevProps) {
-        console.log(this.props);
         if (this.props.dateAttribute && this.props.dateAttribute.status === "available") {
             if (
                 prevProps.dateAttribute !== this.props.dateAttribute &&
@@ -45,25 +57,53 @@ export class DatePickerContainer extends Component {
             if (this.state.placeholder !== this.props.placeholder.value) {
                 this.setState({ placeholder: this.props.placeholder.value });
             }
+        } else if (!this.props.placeholder) {
+            if (this.state.placeholder !== this.state.dateFormat) {
+                this.setState({ placeholder: this.state.dateFormat });
+            }
         }
     }
 
     onChange = newDate => {
-        console.log(newDate);
         this.setState({ dateValue: newDate, editedvalue: newDate });
-        this.props.dateAttribute.setValue(newDate);
+        if (this.isDate(newDate) === true) {
+            this.props.dateAttribute.setValue(newDate);
+        }
+    };
+
+
+    togglePicker = () => {
+        this.setState({ open: !this.state.open });
+    };
+
+    isDate = date => {
+        return moment(date, this.state.dateFormat, true).isValid();
     };
 
     render() {
         return (
-            <DatePicker
-                selected={this.state.dateValue}
-                onChange={this.onChange}
-                showWeekNumbers={this.props.showWeekNumbers}
-                placeholderText={this.state.placeholder}
-                calendarStartDay={this.state.firstDayOfWeek}
-                locale={this.state.locale}
-            />
+            <div className="mx-compound-control" >
+                <DatePicker
+                    selected={this.state.dateValue}
+                    onChange={this.onChange}
+                    showWeekNumbers={this.props.showWeekNumbers}
+                    placeholderText={this.state.placeholder}
+                    calendarStartDay={this.state.firstDayOfWeek}
+                    locale={this.state.locale}
+                    showPopperArrow={false}
+                    onClickOutside={this.togglePicker}
+                    open={this.state.open}
+                    className="form-control"
+                    dateFormat={this.state.dateFormat}
+                    dateFormatCalendar="MMMM"
+                    showYearDropdown={true}
+                    showMonthDropdown={true}
+                    dropdownMode="select"
+                />
+                <button type="button" class="btn mx-button" tabindex="-1" onClick={this.togglePicker}>
+                    <span class="glyphicon glyphicon-calendar"></span>
+                </button>
+            </div>
         );
     }
 }
